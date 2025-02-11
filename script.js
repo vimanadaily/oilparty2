8794d3a0cedb8d282048440f9249d2bed2867b2d
vimanadaily <vibuge@gmail.com>
2025-02-11 17:56:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function search() {
+  const input = document.getElementById('search').value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
 
+  cards.forEach(card => {
+    const title = card.querySelector('h2').innerText.toLowerCase();
+    if (title.includes(input)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
